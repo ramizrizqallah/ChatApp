ed294f652d4b412c090014a34551cf2c7ad52a23
JannatN <jannatfaisalnatsheh@gmail.com>
2020-07-26 22:33:19 +0300

    Sign up

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import LandingPage from './Components/LandingPage.js';
+import RegisterForm from './Components/RegisterForm';
+import './style/common.css';
 
 ReactDOM.render(
   <React.StrictMode>
+    {/* <LandingPage/> */}
+    <RegisterForm />
+
   </React.StrictMode>,
   document.getElementById('root')
 );
-

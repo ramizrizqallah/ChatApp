473ada5788406f4ab4062fc97d9795cba19863f8
maysqunaibi <maysqunaibi2000@gmail.com>
2020-07-26 18:08:40 +0300

    front end landingPage

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import SignIn from './Components/SignIn.js'
 ReactDOM.render(
   <React.StrictMode>
+    <SignIn/>
   </React.StrictMode>,
   document.getElementById('root')
 );

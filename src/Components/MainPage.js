443d520a8f6b199553c411117e4cbb0a5f0e2266
Ramiz Rizqallah <ramiz_rizqallah@hotmail.com>
2020-07-30 18:37:03 +0300

    added main page component and style

---
+++ src/Components/MainPage.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import '../style/mainPage.css'
+import ContactsPanel from './ContactsPanel'
+
+
+export class MainPage extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+
+        }
+    }
+    render() {
+        return (
+            <div className="container">
+                <div className="left-panel">
+
+                    {/* Add navBar Below this line ONLY and put its className ="navbar" */}
+                    
+
+
+                    {/* Add Contacts Panel Below this line ONLY */}
+                    <ContactsPanel />
+
+
+                </div>
+                <div className="right-panel"></div>
+
+            </div>
+        )
+    }
+}
+
+export default MainPage

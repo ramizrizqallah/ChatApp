6265c7892443d1fd3d4c672c703bf6f7aa506c36
JannatN <jannatfaisalnatsheh@gmail.com>
2020-08-02 21:13:59 +0300

    navbar style

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import '../style/mainPage.css'
+import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+
 
 export default function Navbar(props) {
+    const history = useHistory();
     return (
         <div className="navbar">
-            <img style={avatarStyle} src={props.avatar} alt="Avatar" />
+
+            <img id="navAvatar" style={avatarStyle} src={props.avatar} alt="Avatar" onClick={() => window.open("./login", "_blank")} />
             <div className="search-container">
                 <input type="search" results="5" autosave="some_unique_value" placeholder="Search"  ></input>
                 <button id="searchBtn" type="submit"><i className="fa fa-search"></i></button>
